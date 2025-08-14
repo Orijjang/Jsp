e8753986a4f0239d2d5f562b566d65e9c7b56a0f
airforceleather <airforceleather@gmail.com>
2025-08-14 17:48:18 +0900

    add all

---
+++ ErdCollege/src/main/java/controller/register/RequestController.java
@@ -0,0 +1,31 @@
+package controller.register;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import service.RegisterService;
+
+@WebServlet("/register/list.do")
+public class RequestController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	private RegisterService service = RegisterService.INSTANCE;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/register/list.jsp");
+		dispatcher.forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	}
+	
+}
